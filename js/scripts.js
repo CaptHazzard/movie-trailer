71dc813a3e0318294ea65cbc013e5b2096e57dd0
CaptHazzard <59583076+CaptHazzard@users.noreply.github.com>
2020-01-29 14:26:23 -0800

    Added custom controls and styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 console.log('OK');
 
-// Initialize Player
-const player = new Plyr('#player');
+// Initialize player
+const player = new Plyr( '#player', {
+  
+    // Turn on/off conrols. Array data type
+    controls : [ 'play', 'current-time', 'progress', 'volume' ],
+    
+    // Turn on keyboard. Boolean data type
+    keyboard : { focused: true, global: true },
+    
+    // Increase/Decrease seek time. Integer data type
+    seekTime : 15
+  
+  });
